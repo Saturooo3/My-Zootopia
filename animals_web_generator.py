6dddae518b244b2075e96380471b43e0a8501310
Saturooo3 <yusufbulukgiray@gmail.com>
2025-03-02 11:37:05 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,28 +1,45 @@
 import json
 
+
 def load_data(file_path):
   """ Loads a JSON file """
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-animals_data = load_data('animals_data.json')
 
-for animal in animals_data:
-    if animal["name"]:
-        name = animal["name"]
-        print(f"\nName: {name}")
+def read_data():
+    animals_data = load_data('animals_data.json')
+    output = ''
+    for animal in animals_data:
+        if animal["name"]:
+            name = animal["name"]
+            output += f"\nName: {name}\n"
 
-    if animal["locations"][0]:
-        location = animal["locations"][0]
-        print(f"Location: {location}")
+        if animal["locations"][0]:
+            location = animal["locations"][0]
+            output += f"Location: {location}\n"
 
-    if animal["characteristics"]["diet"]:
-        diet = animal["characteristics"]["diet"]
-        print(f"Diet: {diet}")
+        if animal["characteristics"]["diet"]:
+            diet = animal["characteristics"]["diet"]
+            output += f"Diet: {diet}\n"
 
-    if 'type' in animal["characteristics"].keys():
-        animal_type = animal["characteristics"]["type"]
-        print(f"Type: {animal_type}")
+        if 'type' in animal["characteristics"].keys():
+            animal_type = animal["characteristics"]["type"]
+            output += f"Type: {animal_type}\n"
+
+    return output
 
 
+def replace_animals_info():
+    with open("animals_template.html", "r") as f:
+        html_script = f.read()
+    animals_data = read_data()
+    html_script_with_data = html_script.replace("__REPLACE_ANIMALS_INFO__", animals_data)
+    return html_script_with_data
 
+
+def write_data(script):
+    with open("animals.html", "w") as f:
+        f.write(script)
+
+write_data(replace_animals_info())
